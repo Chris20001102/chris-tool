20d703de717efaae3ae3b9fdd3b5e5ab57f6bfd1
Chris20001102 <chris20001102@gmail.com>
2023-02-08 17:58:04 +0800

    navbar url revise

--- src/MyNavbar.js
+++ src/MyNavbar.js
@@ -48,7 +48,7 @@ class MyNavbar extends Component {
                 <Collapse isOpen={!this.state.collapse} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <NavLink href="/test">TEST</NavLink>
+                            <NavLink href="/chris-tool/test">TEST</NavLink>
                         </NavItem>
                         <NavItem>
                             <NavLink href="https://github.com/AidenYang12345">GitHub</NavLink>
