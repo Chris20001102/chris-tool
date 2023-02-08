d490c8e6422131a3262acbdcc27a4a48551e4d56
Chris20001102 <chris20001102@gmail.com>
2023-02-08 17:19:20 +0800

    basic setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from './Home';
+import Test from './Test';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Chris' tool
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} exact={true}></Route>
+          <Route path="/test" component={Test} exact={true}></Route>
+
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Test extends Component {
+
+    render() {
+        return (
+            <div>
+                Test Page
+            </div>
+        );
+    }
+
+}
+
+export default Test;
